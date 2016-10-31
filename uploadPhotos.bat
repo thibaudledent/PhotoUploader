0a9c47250b5188cd22c95959cd2eeac7ca227050
Thibaud Ledent <Thibaud.Ledent@arhs-consulting.com>
2016-10-31 18:48:23 +0100

    uploadPhotos.bat updated: with echo off and automatic updates from repo

--- uploadPhotos.bat
+++ uploadPhotos.bat
@@ -1,2 +1,7 @@
+@echo off 
+echo Check for updates... 
+wget -O PhotoUploader.jar https://github.com/thibaudledent/PhotoUploader/raw/master/out/artifacts/PhotoUploader_jar/PhotoUploader.jar >nul 2>&1
+echo Ready to start the upload? All your photos must be in the current directory
+PAUSE
 start java -jar PhotoUploader.jar
 PAUSE
